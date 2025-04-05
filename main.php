87cd3e01550183e60f9fa574be0955b5f2d9dba3
Ann <annapetukhova18@gmail.com>
2025-04-05 20:06:48 +0300

    ghgh

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Ann\Repo\ChocolateFabrik;
+use Ann\Repo\MiniChocolateFabrik;
+
+$premiumFabrik = new MiniChocolateFabrik();
+$premiumFabrik->orderSnek("Барни");
---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'project/main',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ann/repo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '12f27a6def0ffc3633514d31121713de54512d52',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../ann/repo',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'project/main' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
